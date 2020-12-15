f95f87d6390a8b5b7668d03b36aea7633ba24cac
alexandrenavarro <alexandre.j.navarro@gmail.com>
2020-12-15 22:45:08 +0100

    Add id("de.epitschke.gradle-file-versioning") version "1.0.0"

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import pl.allegro.tech.build.axion.release.domain.TagNameSerializationConfig
-import pl.allegro.tech.build.axion.release.domain.NextVersionConfig
+//import pl.allegro.tech.build.axion.release.domain.TagNameSerializationConfig
+//import pl.allegro.tech.build.axion.release.domain.NextVersionConfig
 
 plugins {
 	id("org.springframework.boot") version "2.4.1"
@@ -8,10 +8,19 @@ plugins {
 	kotlin("jvm") version "1.4.21"
 	kotlin("plugin.spring") version "1.4.21"
 	id("com.google.cloud.tools.jib") version "2.7.0"
-	id("pl.allegro.tech.build.axion-release") version "1.12.0"
+	//id("pl.allegro.tech.build.axion-release") version "1.12.0"
+	//id("eu.davidea.grabver") version "2.0.2"
+	//id("com.karmanno.plugins.semver") version "1.15"
+	//id("com.github.mazzeb.auto-version") version "0.3.0"
+	//id("io.alcide.gradle-semantic-build-versioning") version "4.2.1"
+	id("de.epitschke.gradle-file-versioning") version "1.0.0"
 }
+//
+//autoversion {
+//	versionFile="version.json"
+//}
 
-version = scmVersion.version
+//version = scmVersion.version
 
 group = "com.github.alexandrenavarro"
 java.sourceCompatibility = JavaVersion.VERSION_11
