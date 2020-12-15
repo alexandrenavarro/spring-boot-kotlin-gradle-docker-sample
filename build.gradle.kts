8e445a71d80f8373446e2d3c48218b30cec4c6aa
alexandrenavarro <alexandre.j.navarro@gmail.com>
2020-12-15 22:12:17 +0100

    Some test of release

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ scmVersion {
 		suffix = "SNAPSHOT"
 	})
 }
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-web")
