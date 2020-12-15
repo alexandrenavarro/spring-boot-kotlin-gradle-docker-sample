20c52aff84030758d0a1e97fdcd20b780357ec0c
alexandrenavarro <alexandre.j.navarro@gmail.com>
2020-12-15 22:06:46 +0100

    Some test of release

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,11 +28,7 @@ repositories {
 	mavenCentral()
 }
 
-scmVersion {
-	nextVersion(closureOf<NextVersionConfig> {
-		suffix = ""
-	})
-}
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-web")
