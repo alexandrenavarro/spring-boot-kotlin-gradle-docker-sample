6af73efc762deed0fde2538517ce8b1032be6706
alexandrenavarro <alexandre.j.navarro@gmail.com>
2020-12-15 21:28:08 +0100

    Add Axion plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,6 @@ plugins {
 	kotlin("plugin.spring") version "1.4.21"
 	id("com.google.cloud.tools.jib") version "2.7.0"
 	id("pl.allegro.tech.build.axion-release") version "1.11.0"
-	//id("io.alcide.gradle-semantic-build-versioning") version "4.2.1"
 }
 
 version = scmVersion.version
